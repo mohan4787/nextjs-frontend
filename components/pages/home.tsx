f8745e3884125f55288290a1937ce4680082cc2b
mohan4787 <bishwoshah.it@gmail.com>
2026-04-16 13:28:58 +0545

    resend env added

--- components/pages/home.tsx
+++ components/pages/home.tsx
@@ -13,7 +13,7 @@ const HomePage = () => {
       <div className="flex w-full h-screen items-center justify-center bg-gray-300">
         <div className="flex flex-col gap-5 max-w-3xl mx-auto p-5 bg-gray-100 shadow-xl rounded-md">
           <div className="flex w-full my-4 items-center justify-center rounded-2xl h-25">
-            {/* <img src={logo} alt="Logo" /> */}
+            
           </div>
           <LoginForm/>
         </div>
